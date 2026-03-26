6be14e839a6ae4091e6403e8ef09b4da6fc4e7f5
wsad <muhammad.dafi@taruna-bangsa.sch.id>
2026-03-26 08:37:22 +0700

    langchain

--- until/prompt.py
+++
@@ -1,3 +0,0 @@
-SYSTEM_MESSAGE = """
-hi there you is helpfull asistant
-"""
--- wuti/ai.py
+++ wuti/ai.py
@@ -1,28 +1,35 @@
 import os
-
 from dotenv import load_dotenv
-from google import genai
-from google.genai import types
+from enum import Enum
 
-from until.prompt import SYSTEM_MESSAGE as sm
+from langchain_groq import ChatGroq
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import SystemMessage, HumanMessage
 
 load_dotenv()
-
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 
-def callAi(inputData: str | None = None):
+class Provider(Enum):
+    groq = 'groq'
+    gemini = 'gemini'
+
+def callAi(provider: Provider, inputData: str | None = None, temperature: float = 0.1):
+    if provider == Provider.groq:
+        llm = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct", api_key=GROQ_API_KEY, temperature=temperature)
+    elif provider == Provider.gemini:
+        llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", api_key=GEMINI_API_KEY, temperature=temperature)
+
     if inputData is None:
         return "[WARNING] function 'callAi` no contained input"
-    client = genai.Client(api_key=GEMINI_API_KEY)
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash-lite",
-        config=types.GenerateContentConfig(temperature=0.1, system_instruction=sm),
-        contents=str(inputData),
-    )
-    print(response.text)
+    return llm.invoke([SystemMessage(SYSTEM_MESSAGE), HumanMessage(inputData)]).content
+
+SYSTEM_MESSAGE = """
+hi there you is helpfull asistant
+"""
 
 
 if __name__ == "__main__":
-    callAi()
+    print(callAi(Provider.groq, inputData="hello"))
