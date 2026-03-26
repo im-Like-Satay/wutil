4523c503adb21b7c94508be444ae2adf138391b3
wsad <muhammad.dafi@taruna-bangsa.sch.id>
2026-03-26 08:58:21 +0700

    tambah provider AI

--- main.py
+++
@@ -1,1 +0,0 @@
-from lib.ai import callAi
--- wuti/ai.py
+++ wuti/ai.py
@@ -4,22 +4,36 @@ from enum import Enum
 
 from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
 from langchain_core.messages import SystemMessage, HumanMessage
 
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+ANTROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
 class Provider(Enum):
     groq = 'groq'
     gemini = 'gemini'
+    openai = 'openai'
+    anthropic = 'anthropic'
 
-def callAi(provider: Provider, inputData: str | None = None, temperature: float = 0.1):
-    if provider == Provider.groq:
-        llm = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct", api_key=GROQ_API_KEY, temperature=temperature)
-    elif provider == Provider.gemini:
-        llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", api_key=GEMINI_API_KEY, temperature=temperature)
+PROVIDER = os.getenv("PROVIDER")
+PROVIDER = Provider(PROVIDER)
+
+def callAi(inputData: str | None = None, temperature: float = 0.1):
+    # set model for each provider
+    if PROVIDER == Provider.groq:
+        llm = ChatGroq(model="groq/compound", api_key=GROQ_API_KEY, temperature=temperature)
+    elif PROVIDER == Provider.gemini:
+        llm = ChatGoogleGenerativeAI(model="gemini-3-flash", api_key=GEMINI_API_KEY, temperature=temperature)
+    elif PROVIDER == Provider.openai:
+        llm = ChatOpenAI(model="gpt-5.4", api_key=OPENAI_API_KEY, temperature=temperature)
+    elif PROVIDER == Provider.anthropic:
+        llm = ChatAnthropic(model="claude-sonnet-4-6", api_key=ANTROPIC_API_KEY, temperature=temperature)
 
     if inputData is None:
         return "[WARNING] function 'callAi` no contained input"
@@ -32,4 +46,4 @@ hi there you is helpfull asistant
 
 
 if __name__ == "__main__":
-    print(callAi(Provider.groq, inputData="hello"))
+    print(callAi("hello"))
