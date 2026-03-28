14e9bed90fc07f3b7ef602e668555858f185105a
NotDifined <kakak.jiddan@gmail.com>
2026-03-28 09:23:22 +0700

    tool udah fungsi

--- wuti/parser.py
+++ wuti/parser.py
@@ -7,16 +7,23 @@ def parse_log():
     """fungsi ini untuk memparse log menggunakan regex"""
 
     logData = str(run_wsi())
-    regexPattern = r"'^(?:.*?(?P<error_type>[A-Z][a-zA-Z0-9_]*(?:Error|Exception|Warning|Exit|Interrupt|Stop|NotFound|Failure))\s*:\s*(?P<error_msg>.*))$"
+    regexPattern = r"(?:.*?(?P<error_type>[A-Z][a-zA-Z0-9_]*(?:Error|Exception|Warning|Exit|Interrupt|Stop|NotFound|Failure))\s*:\s*(?P<error_msg>.*))$"
 
     try:
-        resultParserLog = str(re.findall(regexPattern, logData))
+        resultParserLog = re.findall(
+            regexPattern, logData, re.IGNORECASE | re.MULTILINE
+        )
+
+        if not resultParserLog:
+            return None
+
         print(resultParserLog)
-        return resultParserLog
+        return str(resultParserLog)
     except Exception as e:
         print(e)
+        return None
     except KeyboardInterrupt:
-        pass
+        return None
 
 
 if __name__ == "__main__":
